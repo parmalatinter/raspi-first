b4960517556de134137d9a19196a571b1aa7092c
Yosuke.Kobayashi <neo.yosuke.kobayashi@gmail.com>
2018-11-20 11:51:28 +0900

    update method

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='first',
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
+    author='Yosuke Kobayashi',
     author_email='me@kennethreitz.com',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from .context import first
 
 import unittest
 
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+        self.assertEqual(first.get_hmm(), 'hmmm...')
 
 
 if __name__ == '__main__':
